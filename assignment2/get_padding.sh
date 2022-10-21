f71744412cecd26ca43f3a3099cd1671c4b37c0a
crisfj00 <crisfj00@correo.ugr.es>
2022-10-21 11:09:36 +0200

    seccion 4.1 completa

--- assignment2/get_padding.sh
+++ assignment2/get_padding.sh
@@ -1,13 +1,22 @@
 #! /usr/bin/bash
 
 
-echo "Getting message padded of mess1.dat (into padded.dat)"
-total_bytes=`wc -c mess1.dat | cut -f1 -d" "`
+if [ "$#" -ne "1" ]
+then
+echo 'Introduce file with message to pad'
+  echo "$0 file"
+  exit 1
+else
+
+
+echo "Getting message padded from $1 (into padded.dat)"
+total_bytes=`wc -c $1 | cut -f1 -d" "`
 padding=$(( 16 - ($total_bytes%16) ))
 
-cat mess1.dat > padded.dat
+cat $1 > padded.dat
 
 for (( i=0; i<$padding; i++ ))
 do
 	printf "\x$(printf "%02x" "$padding")" >> padded.dat
 done
+fi
