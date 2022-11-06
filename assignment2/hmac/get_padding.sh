265896f532f100dae416fc6d87280650bbde8026
crisfj00 <crisfj00@correo.ugr.es>
2022-11-06 18:10:00 +0100

    simplifación script padding

--- assignment2/hmac/get_padding.sh
+++ assignment2/hmac/get_padding.sh
@@ -22,7 +22,7 @@ else
 	lastblock_bytes=$(( $lastblock_bits / 8 ))
 	bytes_available=$(( $block_size/8 - $lastblock_bytes ))
 	
-	if [[ $lastblock_bits -eq 0 ]] #LAST BLOCK IS COMPLETED (64 BYTES)
+	if [[ $lastblock_bits -eq 0 || $lastblock_bits -le $(( $block_size - $mandatory_bits )) ]] #LAST BLOCK IS COMPLETED (64 BYTES) or FITS MANDATORY PADDING (9 BYTES)
 	then
 
 		necessary_zeros=$(( $bytes_available - $mandatory_bytes ))
@@ -44,14 +44,6 @@ else
 		for run in {1..56}; do printf "\\x$(printf "%x" "0")"; done >> padded.dat  #APPEND 56 BYTES OF 0x00 TO FINISH BLOCK
 		printf "%016x" $total_bits | xxd -r -p >> padded.dat #APPEND SIZE (8 BYTES)
 
-	elif [[ $lastblock_bits -le $(( $block_size - $mandatory_bits )) ]] #LAST BLOCK FITS MANDATORY PADDING (9 BYTES)
-	then
-
-		necessary_zeros=$(( $bytes_available - $mandatory_bytes ))
-
-		printf "\x$(printf "%02x" "128")" >> padded.dat #APPEND 0x80
-		for run in $( seq 1 $necessary_zeros ); do printf "\\x$(printf "%x" "0")"; done >> padded.dat #APPEND NECESSARY 0x00
-		printf "%016x" $total_bits | xxd -r -p >> padded.dat #APPEND SIZE (8 BYTES)
 
 	fi
 fi
