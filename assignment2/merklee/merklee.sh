569db09c0a2a3819a4cc604b88e08184bcaa07fb
pellax <tuxisalive@gmail.com>
2022-11-09 10:44:09 -0500

    falta un nodo

--- assignment2/merklee/merklee.sh
+++ assignment2/merklee/merklee.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
-for i in {0..10};
+for (( i = 0; i < 10; i++))
 do
-    echo hello > "./docs/doc"$i".dat"
+    echo "hello$i" > "./docs/doc"$i".dat"
 done
 division=2
 factor=10
 prefixnode=0xE8
 prefixdoc=0x35
-echo "$prefixnode" >> ./docs/node.pre
-echo "$prefixdoc" >> ./docs/doc.pre
+echo "$prefixnode" > ./docs/node.pre
+echo "$prefixdoc" > ./docs/doc.pre
 #concatenate prefix to doc
-for ((i = 0; i <= $factor; i++))
+for ((i = 0; i < $factor; i++))
 do
-	cat ./docs/doc.pre ./docs/doc$i.dat | openssl dgst -sha1 -binary > "./nodes/node0.$i"
+	cat ./docs/doc.pre ./docs/doc$i.dat | openssl dgst -sha1 -binary | xxd -p > "./nodes/node0.$i"
+	echo -n "0:$i:" >> hashtree.txt 
+	cat ./nodes/node0.$i >> hashtree.txt
 done
-for (( i = 0; i <= 3;i++ ))
+for (( i = 0; i <= 4;i++ ))
 do
-	for (( j = 0; j <= $factor;j++ ))
+	for (( j = 0; j < $factor;j++ ))
 	do	
 		if [[ -f "./nodes/node$i.$(( 2*$j+1 ))" && -f "./nodes/node$i.$(( 2*$j ))" ]]
 		then 
-			cat ./docs/node.pre ./nodes/node$i.$(( 2*$j )) ./nodes/node$i.$(( 2*$j+1 )) | openssl dgst -sha1 -binary > "./nodes/node$(( $i+1 )).$j"
+			cat ./docs/node.pre ./nodes/node$i.$(( 2*$j )) ./nodes/node$i.$(( 2*$j+1 )) | openssl dgst -sha1 -binary | xxd -p > "./nodes/node$(( $i+1 )).$j"
+			echo -n "$(($i+1)):$j: " >> hashtree.txt
+		        cat ./nodes/node$(( $i+1 )).$j >> hashtree.txt	
 		elif [[ -f "./node/node$i.$(( 2*$j ))" ]]	
 		then
-			cat ./docs/node.pre ./nodes/node$i.$(( 2*$j ))  | openssl dgst -sha1 -binary > "./nodes/node$(( $i+1 )).$j"
-		 
+			cat ./docs/node.pre ./nodes/node$i.$(( 2*$j ))  | openssl dgst -sha1 -binary | xxd -p > "./nodes/node$(( $i+1 )).$j" 
+			echo -n "$(($i+1)):$j: " >> hashtree.txt
+		        cat ./nodes/node$(( $i+1 )).$j >> hashtree.txt	
 		fi 	
 	done
 
