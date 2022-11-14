39a818d0ce81bccca4b30bcad30db5861adcec74
pellax <tuxisalive@gmail.com>
2022-11-14 13:55:56 -0500

    casi lo tengo

---
+++ assignment2/merklee/proof.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+if [[ $# -ne 2 ]]
+then 
+	echo "Wrong number of parameters, first parameter is the position, secondo parameter is the node you want to build the proof for "
+else
+for (( i = 0; i < 10; i++))
+do
+    echo "hello$i" > "./docs/doc"$i".dat"
+done
+division=2
+factor=10
+prefixnode=0xE8
+prefixdoc=0x35
+echo -n "$prefixnode" | xxd -p  > ./docs/node.pre
+echo -n "$prefixdoc" | xxd -p  > ./docs/doc.pre
+echo -n "MerkleeTree:sha1:" >> proof.txt
+cat ./docs/node.pre | tr -d '\n' >> proof.txt
+echo -n ":" >> proof.txt
+cat ./docs/doc.pre | tr -d '\n' >> proof.txt
+echo -n ":$factor:" >> proof.txt
+echo -n "5:linux" >> proof.txt
+echo -e '' >> proof.txt
+position=$1
+#concatenate prefix to doc
+for ((i = 0; i < $factor; i++))
+do      
+	if [[ $i -eq $position ]]
+	then 
+		cat ./docs/doc.pre $2 | openssl dgst -sha1 -binary | xxd -p > "./nodes/newnode0.$i"
+		echo -n "0:$i:" >> proof.txt
+		cat ./nodes/newnode0.$i >> proof.txt
+	else
+	cat ./docs/doc.pre ./docs/doc$i.dat | openssl dgst -sha1 -binary | xxd -p > "./nodes/node0.$i"
+	echo -n "0:$i:" >> proof.txt 
+	cat ./nodes/node0.$i >> proof.txt
+	fi
+done
+		        
+sed -i "1 s/linux/$(cat hashtree.txt | grep "MerkleeTree" | cut -d : -f 7)/1" proof.txt
+fi
--- assignment2/merklee/verifydocposition.sh
+++ assignment2/merklee/verifydocposition.sh
@@ -1,80 +1,56 @@
-if [[ "$#" -ne 2 ]] 
+if [[ "$#" -ne 1 ]] 
 then
-    echo "Illegal number of parameters, first parameter defines k position of the check leaf, second parameter is the path of the node you want to check"
+    echo "Illegal number of parameters, first parameter defines the number of docs"
 else
-	cat ./docs/doc.pre $2 | openssl dgst -sha1 -binary | xxd -p  > ./nodes/verifynode0.$1
-	oldroot="$( cat hashtree.txt | grep "MerkleeTree" | cut -d : -f 7 )"
+	factor=$1
+	half=2
+
+
+	oldroot="$( cat proof.txt | grep "MerkleeTree" | cut -d : -f 7 )" #this position holds the original hash of the original merklee tree
 	echo "right way"
-	if [ "$(( 1%2 ))" -eq 0 ];
+
+	elem=$factor
+	
+
+		
+	echo "no entra"
+		
+	#Make copy of the hashtree
+	cat proof.txt > verify.txt  # copy the hash tree 
+	line="$( cat proof.txt | grep 0:$1 )"
+	cat verify.txt
+	#sed -i "s/$line/0:$1:$newleaf/g" proof.txt #substitute the line with the new leaf now lets recalculate the merklee tree
+	for (( i = 0; i < $(( $elem/$half )); i++ )) # taking the original file lets build the whole tree and calculate the hash
+	do
+		for (( j = 0; j <= $factor; j++ ))
+		do	
+			if [[ "$( cat verify.txt | grep -q "$i:$(( 2*$j )):" )" -eq 0 && "$( cat verify.txt | grep -q "$i:$(( 2*$j+1 )):" )" -eq 0  ]]; # we have two nodes
+			then    echo ·existen ambos·
+				newhash="$( cat verify.txt | grep -- "$i:$(( 2*$j )):\|$i:$(( 2*$j+1 )):" | cut -d : -f 3 | tr -d '\n' | openssl dgst -sha1 -binary | xxd -p )" 
+				#sed -i "s/$( cat proof.txt | grep "$(( $i+1 )):$j" | cut -d : -f 3 )/$newhash/g" proof.txt #substitute the hash in the merklee tree
+		                echo -n "$(( $i+1 )):$j:" >> verify.txt
+				echo $newhash >> verify.txt
+			elif [[ "$( cat verify.txt | grep -q "$i:$(( 2*$j )):" )" -eq 0 ]]; #we have one node
+			then    
+				echo "existe uno"
+				newhash="$( cat verify.txt | grep "$i:$(( 2*$j )):" | cut -d : -f 3 | openssl dgst -sha1 -binary | xxd -p )" #get new node hash
+			        echo -n "$(( $i+1 )):$j:" >> verify.txt
+				echo $newhash >> verify.txt
+	
+					#sed -i "s/$( cat proof.txt | grep "$(( $i+1 )):$j" | cut -d : -f 3 )/$newhash/g " proof.txt
+		         fi
+		 done
+		factor=$(( $factor/$half ))
+		echo "$factor"
+	done
+	sed -i "s/$oldroot/$( cat verify.txt | grep "$(( $elem/$half )):0" | cut -d : -f 3 )/g" verify.txt
+	if [[ "$( cat verify.txt | grep "MerkleeTree" | cut -d : -f 7 )" == $( cat proof.txt | grep "MerkleeTree" | cut -d : -f 7 ) ]]
 	then
-		i=0
-		echo "no entra"
-		newleaf="$( cat ./nodes/verifynode0.$1 )"
-		#Make copy of the hashtree
-		cp hashtree.txt proof.txt # copy the hash tree 
-		line="$( cat proof.txt | grep 0:$1 )"
-		cat proof.txt
-		sed -i "s/$line/0:$1:$newleaf/g" proof.txt #substitute the line with the new leaf now lets recalculate the merklee tree
-        	for (( i = 0 ; i < 5; i++ ))
-		do
-			for (( j = 0; j < $factor; j++ ))
-			do	
-				if [[ $( cat proof.txt | grep -q "$i:$(( 2*$j ))" ) &&  $( cat proof.txt | grep -q "$i:$((2*$j+1 ))" ) ]]; # we have two nodes
-				then
-					newhash="$( cat proof.txt | grep -- "$i:$(( 2*$j ))\|$i:$(( 2*$j+1 ))" | cut -d : -f 3 | tr -d '\n' | openssl dgst -sha1 -binary | xxd -p )" 
-					sed -i "s/$( cat proof.txt | grep "$(( $i+1 )):$j" | cut -d : -f 3 )/$newhash/g" proof.txt #substitute the hash in the merklee tree
-				elif [[ $( cat proof.txt | grep -q "$i:$(( 2*$j ))" ) ]]; #we have one node
-				then
-					newhash="$( cat proof.txt | grep "$i:$(( 2*$j ))" | cut -d : -f 3 | openssl dgst -sha1 -binary | xxd -p )" #get new node hash
-			        	sed -i "s/$( cat proof.txt | grep "$(( $i+1 )).$j" | cut -d : -f 3 )/$newhash/g " proof.txt
-		         	fi
-		 	done
-			factor=$(( $factor/2 ))
-			echo "$factor"
-		done
-		sed -i "s/$oldroot/$( cat proof.txt | grep "4:0" | cut -d : -f 3 )/g" proof.txt
-		if [[ "$( cat proof.txt | grep "MerkleeTree" | cut -d : -f 7 )" -eq $oldroot ]]
-		then
-		echo "Verification ok" >> proof.txt
-		else
-			echo "Verification ko" >> proof.txt
-		fi
-
-	else#odd case
- 
-
-		i=0
-		echo "si entra"
-		newleaf="$( cat ./nodes/verifynode0.$1 )"
-		#Make copy of the hashtree
-		cp hashtree.txt proof.txt 
-		cat proof.txt
-		line="$( cat proof.txt | grep 0:$1 )"
-		sed -i "s/$line/0:$1:$newleaf/g" proof.txt #substitute the line with the new leaf now lets recalculate the merklee tree
-        	for (( i = 0 ; i < 5; i++ ))
-		do
-			for (( j = 0; j < $factor; j++ ))
-			do
-				if [[ $( cat proof.txt | grep -q "$i:$(( 2*$j ))" ) &&  $( cat proof.txt | grep -q "$i:$((2*$j+1 ))" ) ]] # we have two nodes
-				then
-					newhash="$( cat proof.txt | grep -- "$i:$(( 2*$j ))\|$i:$(( 2*$j+1 ))" | cut -d : -f 3 | tr -d '\n' | openssl dgst -sha1 -binary | xxd -p )" 
-					sed -i "s/$( cat proof.txt | grep "$(( $i+1 )):$j" | cut -d : -f 3 )/$newhash/g" proof.txt #substitute the hash in the merklee tree
-				elif [[ $( cat proof.txt | grep -q "$i:$(( 2*$j ))" ) ]] #we have one node
-				then
-					newhash="$( cat proof.txt | grep "$i:$(( 2*$j ))" | cut -d : -f 3 | openssl dgst -sha1 -binary | xxd -p )" #get new node hash
-			        	sed -i "s/$( cat proof.txt | grep "$(( $i+1 )).$j" | cut -d : -f 3 )/$newhash/g " proof.txt
-		         	fi
-		 	done
-			factor=$(( $factor/2 ))
-			echo "$factor"
-		done
-		sed -i "s/$oldroot/$( cat proof.txt | grep "4:0" | cut -d : -f 3 )/g" proof.txt
-		if [[ "$( cat proof.txt | grep "MerkleeTree" | cut -d : -f 7 )" -eq $oldroot ]]
-		then	
-		echo "Verification ok" >> proof.txt
-		else
-			echo "Verification ko" >> proof.txt
-        	fi
+	echo "Verification ok" >> verify.txt
+	else
+		echo "Verification ko" >> verify.txt
 	fi
-
 fi
+
+
+
